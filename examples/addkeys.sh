ecaa1ddac6f724632ce6f6d2a83b86120c1fdf95
Cameron Browning <camb416@gmail.com>
2014-11-21 17:08:14 -0500

    readded ruby stuff

--- examples/addkeys.sh
+++ examples/addkeys.sh
@@ -1,2 +1,7 @@
+# add key from processing file
 sed -i .bak s/YOURAPIKEYGOESHERE/$GUGGAPIKEY/g processing_guggenheim/processing_guggenheim.pde
 rm processing_guggenheim/processing_guggenheim.pde.bak
+
+# add key to ruby file
+sed -i .bak s/YOURAPIKEYGOESHERE/$GUGGAPIKEY/g ruby_guggenheim/guggenheim.rb
+rm ruby_guggenheim/guggenheim.rb.bak
--- examples/cleanoutkeys.sh
+++ examples/cleanoutkeys.sh
@@ -1,2 +1,7 @@
+#remove key from processing file
 sed -i .bak s/$GUGGAPIKEY/YOURAPIKEYGOESHERE/g processing_guggenheim/processing_guggenheim.pde
 rm processing_guggenheim/processing_guggenheim.pde.bak
+
+# remove key from ruby file
+sed -i .bak s/$GUGGAPIKEY/YOURAPIKEYGOESHERE/g ruby_guggenheim/guggenheim.rb
+rm ruby_guggenheim/guggenheim.rb.bak
